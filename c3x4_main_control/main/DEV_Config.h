cac3f0e364bc6cb3e0575b6e625cd9d8d64770a9
beijihu <jun.yin@live.com>
2025-12-27 10:48:08 +0800

    Update EPD pin mapping and power analysis based on IDA reverse engineering
    - Correct EPD GPIO pin mapping from IDA analysis:
      RST: GPIO 6, DC: GPIO 5, CS: GPIO 4, BUSY: GPIO 7, MOSI: GPIO 8, SCLK: GPIO 10
    - Add detailed power/power-saving pin analysis section
    - Confirm no dedicated power control GPIO in firmware
    - Provide hardware modification suggestions for power saving

---
+++ c3x4_main_control/main/DEV_Config.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+* | File      	:   DEV_Config.h
+* | Author      :   Waveshare team
+* | Function    :   Hardware underlying interface for ESP32
+* | Info        :
+*                Used to shield the underlying layers of each master
+*                and enhance portability
+*----------------
+* |	This version:   ESP32 V1.0
+* | Date        :   2024-12-26
+******************************************************************************/
+#ifndef _DEV_CONFIG_H_
+#define _DEV_CONFIG_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/**
+ * data
+**/
+#define UBYTE   uint8_t
+#define UWORD   uint16_t
+#define UDOUBLE uint32_t
+
+/**
+ * GPIO config
+**/
+#define EPD_RST_PIN     6
+#define EPD_DC_PIN      5
+#define EPD_CS_PIN      4
+#define EPD_BUSY_PIN    7
+#define EPD_PWR_PIN     -1  // Not used
+#define EPD_MOSI_PIN    8
+#define EPD_SCLK_PIN    10
+
+/*------------------------------------------------------------------------------------------------------*/
+void DEV_Digital_Write(UWORD Pin, UBYTE Value);
+UBYTE DEV_Digital_Read(UWORD Pin);
+
+void DEV_SPI_WriteByte(UBYTE Value);
+void DEV_SPI_Write_nByte(uint8_t *pData, uint32_t Len);
+void DEV_Delay_ms(UDOUBLE xms);
+
+UBYTE DEV_Module_Init(void);
+void DEV_Module_Exit(void);
+
+#endif
