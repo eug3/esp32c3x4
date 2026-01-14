f6ad7dab8372f5dbd5afd6dd4e30f664f5ba51b8
beijihu <jun.yin@live.com>
2026-01-14 09:37:46 +0800

    Refactor code structure for improved readability and maintainability

--- c3x4_main_control/main/version.h
+++ c3x4_main_control/main/version.h
@@ -10,11 +10,11 @@
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       9
 #define VERSION_PATCH       0
-#define VERSION_BUILD       97
-#define VERSION_STRING      "v0.9.0.97-dirty"
-#define VERSION_FULL        "v0.9.0.97-dirty - monster-c3x4 (e126181)"
-#define BUILD_TIME          "2026-01-11 00:39:00"
-#define GIT_COMMIT_HASH     "e126181"
-#define GIT_DIRTY           1
+#define VERSION_BUILD       100
+#define VERSION_STRING      "v0.9.0.100"
+#define VERSION_FULL        "v0.9.0.100 - monster-c3x4 (c864f0d)"
+#define BUILD_TIME          "2026-01-14 09:29:00"
+#define GIT_COMMIT_HASH     "c864f0d"
+#define GIT_DIRTY           0
 
 #endif // VERSION_H
