e6070d1b53d6dad448a752c6e96ed28e8cecac19
beijihu <jun.yin@live.com>
2026-01-25 21:06:10 +0800

    chore(version): update version to v0.9.0.146 and increment build number

--- main/version.h
+++ main/version.h
@@ -10,11 +10,11 @@
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       9
 #define VERSION_PATCH       0
-#define VERSION_BUILD       145
-#define VERSION_STRING      "v0.9.0.145-dirty"
-#define VERSION_FULL        "v0.9.0.145-dirty - monster-c3x4 (8d2fb8f)"
-#define BUILD_TIME          "2026-01-25 19:15:44"
-#define GIT_COMMIT_HASH     "8d2fb8f"
+#define VERSION_BUILD       146
+#define VERSION_STRING      "v0.9.0.146-dirty"
+#define VERSION_FULL        "v0.9.0.146-dirty - monster-c3x4 (ca4efb8)"
+#define BUILD_TIME          "2026-01-25 19:56:09"
+#define GIT_COMMIT_HASH     "ca4efb8"
 #define GIT_DIRTY           1
 
 #endif // VERSION_H
